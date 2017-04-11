fc583b01aa0587ab65f8a4ae35593414a3688970
Asia Kane and Sara Schultz <sara@>
2017-04-11 11:53:56 -0700

    display all neccessary info to user

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ let masterCookBook = new CookBook();
 
 const chickenPotPie = new Recipe (
   "Chicken Pot Pie",
-  "..img/chickenPotPie.jpg",
+  "img/chickenPotPie.jpg",
   "http://www.tasteofhome.com/recipes/favorite-chicken-potpie",
   ["potato", "carrot", "onion", "peas", "corn", "chicken"],
   []
@@ -66,7 +66,7 @@ const chickenPotPie = new Recipe (
 
 const roastChickenAspargus = new Recipe (
   "Roast Chicken With Rhubarb Butter And Asparagus",
-  "..img/roastChickenAspargus.jpg",
+  "img/roastChickenAsparagus.jpg",
   "http://www.bonappetit.com/recipe/roast-chicken-with-rhubarb-butter-and-asparagus",
   ["rhubarb", "chicken", "asparagus", "lemon"],
   ["GF"]
@@ -74,7 +74,7 @@ const roastChickenAspargus = new Recipe (
 
 const beefStuffedEggplant = new Recipe (
   "Stuffed Baby Eggplant with Spiced Ground Beef, Bulgar, and Pine Nuts",
-  "..img/beefStuffedEggplant.jpg",
+  "img/beefStuffedEggplant.jpg",
   "https://www.themediterraneandish.com/stuffed-eggplant-with-spiced-ground-beef-and-bulgur/",
   ["eggplant", "onion", "garlic", "beef", "bulgar", "lemon"],
   []
@@ -82,7 +82,7 @@ const beefStuffedEggplant = new Recipe (
 
 const tofuAsparagusQuiche = new Recipe (
   "Tofu Quiche with Leeks and Asparagus",
-  "..img/tofuAsparagusQuiche.jpg",
+  "img/tofuAsparagusQuiche.jpg",
   "http://www.recipelink.com/cookbooks/2005/1580086187_2.html",
   ["tofu", "asparagus", "garlic", "leek"],
   ["VEG", "DF"]
@@ -90,7 +90,7 @@ const tofuAsparagusQuiche = new Recipe (
 
 const easyVeganFriedRice = new Recipe (
   "Easy Vegan Fried Rice",
-  "..img/easyVeganFriedRice.jpg",
+  "img/easyVeganFriedRice.jpg",
   "http://minimalistbaker.com/easy-vegan-fried-rice/",
   ["tofu", "rice", "garlic", "green onion", "peas", "carrots"],
   ["VEG", "DF", "GF"]
@@ -98,7 +98,7 @@ const easyVeganFriedRice = new Recipe (
 
 const onePotPasta = new Recipe (
   "Sanity-Saving One Pot Pasta",
-  "..img/onePotPasta.jpg",
+  "img/onePotPasta.jpg",
   "https://www.mynewroots.org/site/2016/05/sanity-saving-one-pot-pasta/",
   ["pasta", "asparagus", "peas", "lemon"],
   ["VEG", "DF"]
@@ -106,7 +106,7 @@ const onePotPasta = new Recipe (
 
 const beefStew = new Recipe (
   "Beef Stew with Carrots and Potatoes",
-  "..img/beefStew.jpg",
+  "img/beefStew.jpg",
   "http://www.onceuponachef.com/recipes/beef-stew-with-carrots-potatoes.html",
   ["beef", "onion", "garlic", "carrot", "potato"],
   ["DF"]
@@ -114,7 +114,7 @@ const beefStew = new Recipe (
 
 const blackBeanEnchilada = new Recipe (
   "Veggie Black Bean Enchiladas",
-  "..img/blackBeanEnchilada.jpg",
+  "img/blackBeanEnchilada.jpg",
   "http://cookieandkate.com/2016/vegetarian-enchiladas-recipe/",
   ["onion", "pepper", "broccoli", "spinach"],
   ["VEG"]
@@ -122,7 +122,7 @@ const blackBeanEnchilada = new Recipe (
 
 const sobaNoodleChicken = new Recipe (
   "Soba Noodle Chicken Stir Fry",
-  "..img/sobaNoodleChicken.jpg",
+  "img/sobaNoodleChicken.jpg",
   "http://www.cookforyourlife.org/recipes/soba-noodle-chicken-stir-fry/",
   ["pasta", "chicken", "carrot", "onion", "pepper", "cabbage"],
   ["DF"]
@@ -130,7 +130,7 @@ const sobaNoodleChicken = new Recipe (
 
 const beefAndNoodles = new Recipe (
   "Beef and Noodles",
-  "..img/beefandnoodles.jpg",
+  "img/beefandnoodles.jpg",
   "http://www.gonnawantseconds.com/2017/03/beef-and-noodles/",
   ["beef", "onion", "garlic", "pasta"],
   ["DF"]
@@ -138,7 +138,7 @@ const beefAndNoodles = new Recipe (
 
 const tofuWildRiceSoup = new Recipe (
   "Rustic Tofu Wild Rice Soup",
-  "..img/tofu-wild-rice-soup.jpg",
+  "img/tofu-wild-rice-soup.jpg",
   "http://heartbeetkitchen.com/2017/recipes/rustic-tofu-wild-rice-soup/",
   ["tofu", "celery", "onion", "carrot", "mushroom", "rice"],
   ["VEG", "GF"]
@@ -146,7 +146,7 @@ const tofuWildRiceSoup = new Recipe (
 
 const tofuAsparagus = new Recipe (
   "Chili-Glazed tofu over Asparagus and Rice",
-  "..img/tofu-asparagus.jpg",
+  "img/tofu-asparagus.jpg",
   "http://www.myrecipes.com/recipe/chili-glazed-tofu-over-asparagus-rice",
   ["tofu", "asparagus", "onion", "carrot", "rice"],
   ["VEG", "DF"]
@@ -154,7 +154,7 @@ const tofuAsparagus = new Recipe (
 
 const tikkaMasala = new Recipe (
   "Chicken Tikka Masala",
-  "..img/tikka-masala.jpg",
+  "img/tikka-masala.jpg",
   "http://www.savorytooth.com/instant-pot-chicken-tikka-masala/",
   ["chicken", "rice", "garlic"],
   ["GF"]
@@ -162,7 +162,7 @@ const tikkaMasala = new Recipe (
 
 const mapoTofu = new Recipe (
   "Mapo Tofu",
-  "..img/Mapo-tofu.jpg",
+  "img/Mapo-tofu.jpg",
   "https://redhousespice.com/mapo-tofu-authentic-way/",
   ["beef", "tofu", "rice", "onion"],
   ["DF"]
@@ -170,7 +170,7 @@ const mapoTofu = new Recipe (
 
 const sheetChicken = new Recipe (
   "Lemony Herb Sheet Pan Chicken & Vegetables",
-  "..img/sheet-pan.jpg",
+  "img/sheet-pan.jpg",
   "http://delightfulmomfood.com/sheet-pan-chicken-vegetables/",
   ["chicken", "carrot", "broccoli", "potato", "cauliflower", "garlic"],
   ["DF", "GF"]
@@ -178,7 +178,7 @@ const sheetChicken = new Recipe (
 
 const beefAndBroccoli = new Recipe (
   "Beef and Broccoli",
-  "..img/beef-broccoli.jpg",
+  "img/beef-broccoli.jpg",
   "http://www.food.com/recipe/the-best-easy-beef-and-broccoli-stir-fry-99476",
   ["beef", "onion", "rice", "broccoli"],
   ["DF", "GF"]
@@ -186,7 +186,7 @@ const beefAndBroccoli = new Recipe (
 
 const macAndCheese = new Recipe (
   "Mac and Cheese From Scratch",
-  "..img/mac.jpg",
+  "img/mac.jpg",
   "http://www.myrecipes.com/recipe/classic-baked-macaroni-cheese",
   ["pasta"],
   ["VEG"]
@@ -194,7 +194,7 @@ const macAndCheese = new Recipe (
 
 const loadedPotato = new Recipe (
   "Loaded Chili-Stuffed Baked Potatoes",
-  "..img/Loaded-Potato.jpg",
+  "img/Loaded-Potato.jpg",
   "http://wickedhealthyfood.com/2017/02/07/loaded-chili-stuffed-baked-potatoes/",
   ["potato", "onion", "cabbage", "kale"],
   ["VEG", "GF"]
@@ -222,10 +222,26 @@ $(function(){
       }
     }
     newUser.recipeMatches = (masterCookBook.whatIngredients(newUser.ingredients));
+    console.log(newUser.recipeMatches);
     $("#recipe-results").empty();
     for(var i=0; i<newUser.recipeMatches.length; i++) {
-      console.log(newUser.recipeMatches[i][0].title);
-      $("#recipe-results").append(newUser.recipeMatches[i][0].title);
+      let recipeTitle = newUser.recipeMatches[i][0].title;
+      let recipeLink = newUser.recipeMatches[i][0].link;
+      let recipeImage = newUser.recipeMatches[i][0].image;
+      $("ul#recipe-results").append(`<a href="${recipeLink} target="_blank">${recipeTitle}</a> \n <img src=${recipeImage}>`);
+      for (var j=1; j<newUser.recipeMatches[i].length; j++){
+        let matchedIngredients = newUser.recipeMatches[i][j];
+        $("ul#recipe-results").append("<li> Includes: " + matchedIngredients + "</li>");
+      }
+
+
+
+      // $("ul#recipe-results").append(function() {
+      //   for (var j=1; j<newUser.recipeMatches.length; j++){
+      //   return "<li>" + newUser.recipeMatches[i][0].title + "</li> contains  "
+      //   + newUser.recipeMatches[i][j] + "!"
+      //   }
+      // });
     }
   });
 
