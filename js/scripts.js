493b978a3663d8e20296575ae89c3a74484da68e
Asia Kane and Sara Schultz <sara@>
2017-04-13 09:27:43 -0700

    add comments in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,14 @@ class CookBook {
     this.recipes.push(recipe);
   }
 
+//Loop for dietary restrictions which returns a boolean called by getRecipesByRestriction
   isAcceptableRecipe(recipe, userDietaryRestrictions){
     return userDietaryRestrictions.every(function(userRestriction) {
       return recipe.dietaryRestrictions.includes(userRestriction);
     });
   }
 
+  //Push only recipes that meet dietary restrictions into acceptableRecipes array
   getRecipesByRestriction(userDietaryRestrictions){
     if (userDietaryRestrictions.length === 0) {
       return this.recipes;
@@ -42,29 +44,33 @@ class CookBook {
       if (this.isAcceptableRecipe(recipe, userDietaryRestrictions)) {
         acceptableRecipes.push(recipe);
       }
-    }, this);
+    }, this); //Passes in parent object Cookbook into for each loop
     return acceptableRecipes;
   }
 
-  matches(ingredients, recipes) {
+  //Creates matchedRecipes array of recipes that match user inputted ingredients
+  getRecipesByIngredients(ingredients, recipes) {
     let matchedRecipes = [];
     for (let i = 0; i < recipes.length; i++) {
       let recipe = recipes[i];
       let recipeIngredients = recipes[i].ingredients;
       //Return all recipes that match ingredients
       ingredients.forEach(function(ingredient){
+        //Checks if ingredient is included in recipe's ingredients and if ingredient is not already in the matchedRecipes array
         if (recipeIngredients.includes(ingredient) && !matchedRecipes.includes(recipe)){
           matchedRecipes.push(recipe);
         }
       });
     }
+    //Creates an array for each recipe within the matchedRecipes array
     return matchedRecipes.map(function(matchedRecipe){
       return [matchedRecipe];
     });
   }
 
-  getRecipesByIngredient(ingredients, recipes) {
-    let matchedRecipes = this.matches(ingredients, recipes);
+  //Takes matchedRecipes array and pushes only matched ingredients into each recipe array.  Then sort.
+  getMatchedIngredients(ingredients, recipes) {
+    let matchedRecipes = this.getRecipesByIngredients(ingredients, recipes);
     for(var i = 0; i < matchedRecipes.length; i++){
       ingredients.forEach(function(ingredient){
         if (matchedRecipes[i][0].ingredients.includes(ingredient)) {
@@ -77,17 +83,17 @@ class CookBook {
     });
   }
 
+  //Returns final matched recipes according to user inputted ingredients and dietary restrictions
   getAllAcceptableRecipes(ingredients, userDietaryRestrictions) {
-    return this.getRecipesByIngredient(ingredients, this.getRecipesByRestriction(userDietaryRestrictions));
+    return this.getMatchedIngredients(ingredients, this.getRecipesByRestriction(userDietaryRestrictions));
   }
 } //Close cookbook class
 
 // *************User Logic **********
 
 $(function(){
-  var newUser = new User();
 
-  var getIt = function(){
+  var displayResults = function(){
     newUser.recipeMatches = (masterCookBook.getAllAcceptableRecipes(newUser.ingredients, newUser.dietaryRestrictions));
     $("#recipe-results").empty();
     for(var i=0; i<newUser.recipeMatches.length; i++) {
@@ -97,6 +103,7 @@ $(function(){
       let recipeRestrictions = newUser.recipeMatches[i][0].dietaryRestrictions;
       $("ul#recipe-results").append(`<a class="recipeTitle" href="${recipeLink}" target="_blank">${recipeTitle} <br> <img class="recipeImage" src=${recipeImage}></a><br>`);
 
+      //to show dietary restriction icon for the recipe, if applicable
       if (recipeRestrictions.length > 0) {
         recipeRestrictions.forEach(function(restriction){
           if (restriction === "GF") {
@@ -112,6 +119,7 @@ $(function(){
         $("ul#recipe-results").append("<br>" );
       }
 
+      //loops through matched recipes and adds the included ingredients and recipe separator icon to the result; if there's no result, display an empty plate with message. The number of results is also added.
       $("ul#recipe-results").append("<li> Includes: </li>");
       for (var j=1; j<newUser.recipeMatches[i].length; j++){
         let matchedIngredients = newUser.recipeMatches[i][j];
@@ -123,7 +131,9 @@ $(function(){
       $("#recipe-results").append(`<h3>Aww. Shucks. You didn't match any results.</h3> <img id="empty-image" src="img/empty.jpg">`);
     }
     $("#number-of-results").text(newUser.recipeMatches.length);
-  }
+  } //ends displayResults function
+
+  var newUser = new User();
 
   //Collect user input ingredients and push to newUser
   $('input[name="ingredients"]').on('click', function () {
@@ -132,11 +142,11 @@ $(function(){
     if ($(this).is(':checked')) {
       newUser.ingredients.push($(this).val());
     } else {
-      if ((index =  newUser.ingredients.indexOf($(this).val())) !== -1) {
-        newUser.ingredients.splice(index, 1);
+        if ((index = newUser.ingredients.indexOf($(this).val())) !== -1) {
+          newUser.ingredients.splice(index, 1);
+        }
       }
-    }
-    getIt();
+    displayResults();
   });
 
   //For collecting user input dietary restrictions and push to newUser.
@@ -147,25 +157,15 @@ $(function(){
     if ($(this).is(':checked')) {
       newUser.dietaryRestrictions.push($(this).val());
     } else {
-      if ((index = newUser.dietaryRestrictions.indexOf($(this).val())) !== -1) {
-        newUser.dietaryRestrictions.splice(index, 1);
+        if ((index = newUser.dietaryRestrictions.indexOf($(this).val())) !== -1) {
+          newUser.dietaryRestrictions.splice(index, 1);
+        }
       }
-    }
-    getIt();
+    displayResults();
   });
+
   $("#reset-button").click(function() {
     location.reload();
   });
 
-
-  //Jumbotron parallax
-  var jumboHeight = $('.jumbotron').outerHeight();
-  function parallax(){
-      var scrolled = $(window).scrollTop();
-      $('.jumbotron').css('height', (jumboHeight-scrolled) + 'px');
-  }
-
-  $(window).scroll(function(e){
-      parallax();
-  });
 });//ends doc ready
